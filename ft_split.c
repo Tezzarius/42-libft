0c9d9a226e8cce39d72982b6e4fbfbd2294e0db8
Tezzarius <mortis@live.at>
2025-05-09 12:17:11 +0200

    testing

---
+++ ft_split.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bschwarz <bschwarz@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/01 14:10:22 by bschwarz          #+#    #+#             */
+/*   Updated: 2025/05/08 14:09:50 by bschwarz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static void	free_array(char **arr, int count)
+{
+	while (--count >= 0)
+		free(arr[count]);
+		free(arr);
+}
+
+static int	ft_wordcount(char const *s, int c)
+{
+	int	count;
+	int	sw;
+	int	i;
+
+	i = 0;
+	sw = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] != c && sw == 0)
+		{
+			count++;
+			sw = 1;
+		}
+		else if (s[i] == c)
+			sw = 0;
+		i++;
+	}
+	return (count);
+}
+
+static int	**fillarray(char **dest, char const *s, char c)
+{
+	int		i;
+	int		j;
+	int		start;
+
+	j = 0;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			start = i;
+			while (s[i] && s[i] != c)
+				i++;
+			dest[j] = ft_substr(&s, start, i - start);
+			if (!dest[j])
+				return (free_array(dest, j), 0);
+			j++;
+		}
+		else
+			i++;
+	}
+	dest[j] = NULL;
+	return (1);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**dest;
+
+	if (!s)
+		return (NULL);
+	dest = malloc((ft_wordcount(s, c) + 1) * sizeof(char *));
+	if (!dest)
+		return (NULL);	
+	if (!fillarray(dest, s, c))
+		return (NULL);
+	return (dest);
+}
+
+/* #include <stdio.h>
+
+int main()
+{
+	char *string = "      split       this for   me  !       ";
+	char **result = ft_split(string, ' ');
+	printf("%s\n", result[0]);
+	printf("%s\n", result[1]);
+	printf("%s\n", result[2]);
+	printf("%s\n", result[3]);
+	printf("%s\n", result[4]);
+	printf("%s\n", result[5]);
+	return (0);
+} */
---
+++ ft_strlcat.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bschwarz <bschwarz@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 09:24:30 by bschwarz          #+#    #+#             */
+/*   Updated: 2025/05/08 14:11:09 by bschwarz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strlcat(char *dest, char *src, size_t size)
+{
+	size_t	destlen;
+	size_t	srclen;
+	size_t	i;
+
+	srclen = ft_strlen(src);
+	destlen = 0;
+	while (destlen < size && dest[destlen])
+		destlen++;
+	if (destlen == size)
+		return (destlen + srclen);
+	i = 0;
+	while (src[i] && (destlen + i + 1) < size)
+	{
+		dest[destlen + i] = src[i];
+		i++;
+	}
+	dest[destlen + i] = '\0';
+	return (destlen + srclen);
+}
+
+/* #include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char str[14] = "i";
+
+	printf("%d\n", ft_strlcat(str, " don't know what i should write....", 15));
+	printf("%s\n", str);
+	return (0);
+} */
